30ba6e1ce34bf231cf075098a95e0a49b0d913b5
Francesco Strazzullo <me@francescostrazzullo.info>
2014-01-28 21:42:50 +0100

    Deleted Primefaces Commandbuttons

--- src/main/java/it/strazz/TestBean.java
+++ src/main/java/it/strazz/TestBean.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 
 import org.omnifaces.util.Components;
 
@@ -12,7 +13,9 @@ import org.omnifaces.util.Components;
 @SessionScoped
 public class TestBean implements Serializable{
  
-	public void add(){
+	private static final long serialVersionUID = 1L;
+
+	public void add(ActionEvent event){
 		Components.includeCompositeComponent(Components.findComponent("myForm"), "strazz", "test.xhtml", FacesContext.getCurrentInstance().getViewRoot().createUniqueId());
 	}
 	
